5a822b7e1c9d3011b16f75681a5e4c7c4697c7ec
MrCoconutman <59306156+MrCoconutman@users.noreply.github.com>
2019-12-28 09:47:41 +0000

    Add files via upload

---
+++ Question4.java
@@ -0,0 +1,17 @@
+package assignements.day_2;
+
+public class Question4 
+
+{
+public static void main(String[] arg) {
+	
+
+
+System.out.println("Q");
+
+  }
+}
+
+ 
+
+	
---
+++ Question8.java
@@ -0,0 +1,18 @@
+package assignements.day_2;
+
+public class Question8 {
+	public static void main(String[] args) {
+		
+		System.out.println("        *               *           ***********");
+		System.out.println("       ***             ***          ***********");
+		System.out.println("      ** **           ** **         **");
+		System.out.println("     **   **         **   **        **");
+		System.out.println("    **     **       **     **       **");
+		System.out.println("   **       **     **       **      **");
+		System.out.println("  **         **   **         **     **");
+		System.out.println(" **            ****           **    ***********");
+		System.out.println("**              **             **   ***********");
+
+
+	}
+}
